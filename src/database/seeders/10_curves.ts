92e09299c183e699a6d0969372fa8601d91ed8ea
Facundo Rodriguez <rodriguezfacundohernan@gmail.com>
2021-05-13 09:11:15 -0300

    Added seeders for IBP curve

--- src/database/seeders/10_curves.ts
+++ src/database/seeders/10_curves.ts
@@ -89,6 +89,64 @@ export default class CreateCurves implements Seeder {
             insert into simvet.ppcurve (t, value, id_as, id_pp, id_scenario) values (0.964091403699673, 34.9282296650717, 1, 2, 1);
             insert into simvet.ppcurve (t, value, id_as, id_pp, id_scenario) values (0.997823721436343, 35.645933014354, 1, 2, 1);`);
 
+      await connection.query(`INSERT INTO simvet.ppcurve (t, value, created_at, updated_at, id_as, id_pp, id_scenario) VALUES (-0.000406417868249926, 58.4107694003527, '2021-05-13 12:03:11.845378', '2021-05-13 12:03:11.845378', 1, 1, 1);
+INSERT INTO simvet.ppcurve (t, value, created_at, updated_at, id_as, id_pp, id_scenario) VALUES (0.0180001884836491, 60.3053350970017, '2021-05-13 12:03:11.845378', '2021-05-13 12:03:11.845378', 1, 1, 1);
+INSERT INTO simvet.ppcurve (t, value, created_at, updated_at, id_as, id_pp, id_scenario) VALUES (0.0272034916595985, 62.7166005291005, '2021-05-13 12:03:11.845378', '2021-05-13 12:03:11.845378', 1, 1, 1);
+INSERT INTO simvet.ppcurve (t, value, created_at, updated_at, id_as, id_pp, id_scenario) VALUES (0.0345661342003581, 65.644565696649, '2021-05-13 12:03:11.845378', '2021-05-13 12:03:11.845378', 1, 1, 1);
+INSERT INTO simvet.ppcurve (t, value, created_at, updated_at, id_as, id_pp, id_scenario) VALUES (0.0419287767411177, 68.2280643738977, '2021-05-13 12:03:11.845378', '2021-05-13 12:03:11.845378', 1, 1, 1);
+INSERT INTO simvet.ppcurve (t, value, created_at, updated_at, id_as, id_pp, id_scenario) VALUES (0.0456100980114975, 71.8449625220458, '2021-05-13 12:03:11.845378', '2021-05-13 12:03:11.845378', 1, 1, 1);
+INSERT INTO simvet.ppcurve (t, value, created_at, updated_at, id_as, id_pp, id_scenario) VALUES (0.0492914192818773, 75.1173941798942, '2021-05-13 12:03:11.845378', '2021-05-13 12:03:11.845378', 1, 1, 1);
+INSERT INTO simvet.ppcurve (t, value, created_at, updated_at, id_as, id_pp, id_scenario) VALUES (0.0511320799170672, 77.8731261022928, '2021-05-13 12:03:11.845378', '2021-05-13 12:03:11.845378', 1, 1, 1);
+INSERT INTO simvet.ppcurve (t, value, created_at, updated_at, id_as, id_pp, id_scenario) VALUES (0.054813401187447, 80.2843915343915, '2021-05-13 12:03:11.845378', '2021-05-13 12:03:11.845378', 1, 1, 1);
+INSERT INTO simvet.ppcurve (t, value, created_at, updated_at, id_as, id_pp, id_scenario) VALUES (0.0603353830930167, 82.5234237213404, '2021-05-13 12:03:11.845378', '2021-05-13 12:03:11.845378', 1, 1, 1);
+INSERT INTO simvet.ppcurve (t, value, created_at, updated_at, id_as, id_pp, id_scenario) VALUES (0.0658573649985864, 85.7958553791887, '2021-05-13 12:03:11.845378', '2021-05-13 12:03:11.845378', 1, 1, 1);
+INSERT INTO simvet.ppcurve (t, value, created_at, updated_at, id_as, id_pp, id_scenario) VALUES (0.0695386862689662, 89.4127535273368, '2021-05-13 12:03:11.845378', '2021-05-13 12:03:11.845378', 1, 1, 1);
+INSERT INTO simvet.ppcurve (t, value, created_at, updated_at, id_as, id_pp, id_scenario) VALUES (0.0713793469041561, 95.4409171075838, '2021-05-13 12:03:11.845378', '2021-05-13 12:03:11.845378', 1, 1, 1);
+INSERT INTO simvet.ppcurve (t, value, created_at, updated_at, id_as, id_pp, id_scenario) VALUES (0.0805826500801056, 99.9189814814815, '2021-05-13 12:03:11.845378', '2021-05-13 12:03:11.845378', 1, 1, 1);
+INSERT INTO simvet.ppcurve (t, value, created_at, updated_at, id_as, id_pp, id_scenario) VALUES (0.0842639713504854, 104.224812610229, '2021-05-13 12:03:11.845378', '2021-05-13 12:03:11.845378', 1, 1, 1);
+INSERT INTO simvet.ppcurve (t, value, created_at, updated_at, id_as, id_pp, id_scenario) VALUES (0.0897859532560551, 109.564043209877, '2021-05-13 12:03:11.845378', '2021-05-13 12:03:11.845378', 1, 1, 1);
+INSERT INTO simvet.ppcurve (t, value, created_at, updated_at, id_as, id_pp, id_scenario) VALUES (0.108192559607954, 117.486772486772, '2021-05-13 12:03:11.845378', '2021-05-13 12:03:11.845378', 1, 1, 1);
+INSERT INTO simvet.ppcurve (t, value, created_at, updated_at, id_as, id_pp, id_scenario) VALUES (0.126599165959853, 120.070271164021, '2021-05-13 12:03:11.845378', '2021-05-13 12:03:11.845378', 1, 1, 1);
+INSERT INTO simvet.ppcurve (t, value, created_at, updated_at, id_as, id_pp, id_scenario) VALUES (0.148687093582132, 117.831238977072, '2021-05-13 12:03:11.845378', '2021-05-13 12:03:11.845378', 1, 1, 1);
+INSERT INTO simvet.ppcurve (t, value, created_at, updated_at, id_as, id_pp, id_scenario) VALUES (0.159731057393271, 114.731040564374, '2021-05-13 12:03:11.845378', '2021-05-13 12:03:11.845378', 1, 1, 1);
+INSERT INTO simvet.ppcurve (t, value, created_at, updated_at, id_as, id_pp, id_scenario) VALUES (0.170775021204411, 111.630842151675, '2021-05-13 12:03:11.845378', '2021-05-13 12:03:11.845378', 1, 1, 1);
+INSERT INTO simvet.ppcurve (t, value, created_at, updated_at, id_as, id_pp, id_scenario) VALUES (0.191022288191499, 108.358410493827, '2021-05-13 12:03:11.845378', '2021-05-13 12:03:11.845378', 1, 1, 1);
+INSERT INTO simvet.ppcurve (t, value, created_at, updated_at, id_as, id_pp, id_scenario) VALUES (0.202066252002639, 103.70811287478, '2021-05-13 12:03:11.845378', '2021-05-13 12:03:11.845378', 1, 1, 1);
+INSERT INTO simvet.ppcurve (t, value, created_at, updated_at, id_as, id_pp, id_scenario) VALUES (0.214950876448968, 100.263447971781, '2021-05-13 12:03:11.845378', '2021-05-13 12:03:11.845378', 1, 1, 1);
+INSERT INTO simvet.ppcurve (t, value, created_at, updated_at, id_as, id_pp, id_scenario) VALUES (0.224154179624918, 97.8521825396825, '2021-05-13 12:03:11.845378', '2021-05-13 12:03:11.845378', 1, 1, 1);
+INSERT INTO simvet.ppcurve (t, value, created_at, updated_at, id_as, id_pp, id_scenario) VALUES (0.237038804071247, 94.5797508818342, '2021-05-13 12:03:11.845378', '2021-05-13 12:03:11.845378', 1, 1, 1);
+INSERT INTO simvet.ppcurve (t, value, created_at, updated_at, id_as, id_pp, id_scenario) VALUES (0.251764089152766, 91.135085978836, '2021-05-13 12:03:11.845378', '2021-05-13 12:03:11.845378', 1, 1, 1);
+INSERT INTO simvet.ppcurve (t, value, created_at, updated_at, id_as, id_pp, id_scenario) VALUES (0.262808052963905, 89.068287037037, '2021-05-13 12:03:11.845378', '2021-05-13 12:03:11.845378', 1, 1, 1);
+INSERT INTO simvet.ppcurve (t, value, created_at, updated_at, id_as, id_pp, id_scenario) VALUES (0.277533338045425, 85.6236221340388, '2021-05-13 12:03:11.845378', '2021-05-13 12:03:11.845378', 1, 1, 1);
+INSERT INTO simvet.ppcurve (t, value, created_at, updated_at, id_as, id_pp, id_scenario) VALUES (0.306983908208463, 83.9012896825397, '2021-05-13 12:03:11.845378', '2021-05-13 12:03:11.845378', 1, 1, 1);
+INSERT INTO simvet.ppcurve (t, value, created_at, updated_at, id_as, id_pp, id_scenario) VALUES (0.35300042408821, 84.0735229276896, '2021-05-13 12:03:11.845378', '2021-05-13 12:03:11.845378', 1, 1, 1);
+INSERT INTO simvet.ppcurve (t, value, created_at, updated_at, id_as, id_pp, id_scenario) VALUES (0.402698261238338, 85.279155643739, '2021-05-13 12:03:11.845378', '2021-05-13 12:03:11.845378', 1, 1, 1);
+INSERT INTO simvet.ppcurve (t, value, created_at, updated_at, id_as, id_pp, id_scenario) VALUES (0.450555437753275, 82.3511904761905, '2021-05-13 12:03:11.845378', '2021-05-13 12:03:11.845378', 1, 1, 1);
+INSERT INTO simvet.ppcurve (t, value, created_at, updated_at, id_as, id_pp, id_scenario) VALUES (0.483687329186693, 80.8010912698413, '2021-05-13 12:03:11.845378', '2021-05-13 12:03:11.845378', 1, 1, 1);
+INSERT INTO simvet.ppcurve (t, value, created_at, updated_at, id_as, id_pp, id_scenario) VALUES (0.513137899349732, 77.7008928571428, '2021-05-13 12:03:11.845378', '2021-05-13 12:03:11.845378', 1, 1, 1);
+INSERT INTO simvet.ppcurve (t, value, created_at, updated_at, id_as, id_pp, id_scenario) VALUES (0.54258846951277, 75.461860670194, '2021-05-13 12:03:11.845378', '2021-05-13 12:03:11.845378', 1, 1, 1);
+INSERT INTO simvet.ppcurve (t, value, created_at, updated_at, id_as, id_pp, id_scenario) VALUES (0.586764324757327, 72.8783619929453, '2021-05-13 12:03:11.845378', '2021-05-13 12:03:11.845378', 1, 1, 1);
+INSERT INTO simvet.ppcurve (t, value, created_at, updated_at, id_as, id_pp, id_scenario) VALUES (0.625418198096315, 71.500496031746, '2021-05-13 12:03:11.845378', '2021-05-13 12:03:11.845378', 1, 1, 1);
+INSERT INTO simvet.ppcurve (t, value, created_at, updated_at, id_as, id_pp, id_scenario) VALUES (0.671434713976063, 69.9503968253968, '2021-05-13 12:03:11.845378', '2021-05-13 12:03:11.845378', 1, 1, 1);
+INSERT INTO simvet.ppcurve (t, value, created_at, updated_at, id_as, id_pp, id_scenario) VALUES (0.71745122985581, 67.8835978835979, '2021-05-13 12:03:11.845378', '2021-05-13 12:03:11.845378', 1, 1, 1);
+INSERT INTO simvet.ppcurve (t, value, created_at, updated_at, id_as, id_pp, id_scenario) VALUES (0.757945763829988, 66.1612654320987, '2021-05-13 12:03:11.845378', '2021-05-13 12:03:11.845378', 1, 1, 1);
+INSERT INTO simvet.ppcurve (t, value, created_at, updated_at, id_as, id_pp, id_scenario) VALUES (0.789236994628216, 65.3000992063492, '2021-05-13 12:03:11.845378', '2021-05-13 12:03:11.845378', 1, 1, 1);
+INSERT INTO simvet.ppcurve (t, value, created_at, updated_at, id_as, id_pp, id_scenario) VALUES (0.837094171143154, 63.75, '2021-05-13 12:03:11.845378', '2021-05-13 12:03:11.845378', 1, 1, 1);
+INSERT INTO simvet.ppcurve (t, value, created_at, updated_at, id_as, id_pp, id_scenario) VALUES (0.89415465083404, 62.3721340388007, '2021-05-13 12:03:11.845378', '2021-05-13 12:03:11.845378', 1, 1, 1);
+INSERT INTO simvet.ppcurve (t, value, created_at, updated_at, id_as, id_pp, id_scenario) VALUES (0.958577773065687, 60.9942680776014, '2021-05-13 12:03:11.845378', '2021-05-13 12:03:11.845378', 1, 1, 1);
+            INSERT INTO simvet.ppcurve (t, value, created_at, updated_at, id_as, id_pp, id_scenario) VALUES (1.00091296767505, 60.3053350970017, '2021-05-13 12:03:11.845378', '2021-05-13 12:03:11.845378', 1, 1, 1);`)
+
+
+
+      // let sinValue: number;
+      // let cosValue: number;
+      // for (let i: number = 0.00; i < 1.0; i += 0.01) {
+      //   sinValue = Math.sin(i + 45);
+      //   cosValue = Math.cos(i + 45);
+      //   await connection.query(`insert into simvet.ppcurve (t, value, id_as, id_pp, id_scenario) values (${i}, ${sinValue}, 1, 1, 1); `)
+      //   // await connection.query(`insert into simvet.ppcurve (t, value, id_as, id_pp, id_scenario) values (${i}, ${cosValue}, 1, 4, 1); `)
+
+      // }
       return;
     } catch (error) {
       console.log(error);
